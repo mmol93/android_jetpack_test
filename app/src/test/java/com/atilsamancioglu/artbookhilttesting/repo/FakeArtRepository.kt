ab08cac32bc13f9f1d5a7743df70c6b12ad88f3d
ljws93 <ljws93@naver.com>
2023-01-16 22:42:47 +0900

    init commit but add viewModel test

---
+++ app/src/test/java/com/atilsamancioglu/artbookhilttesting/repo/FakeArtRepository.kt
@@ -0,0 +1,44 @@
+package com.atilsamancioglu.artbookhilttesting.repo
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.atilsamancioglu.artbookhilttesting.model.ImageResponse
+import com.atilsamancioglu.artbookhilttesting.roomdb.Art
+import com.atilsamancioglu.artbookhilttesting.util.Resource
+
+/**
+ * viewModel 테스트에 사용하기 위한 Fake Repository 클래스
+ * 이 Repository의 목적은 viewModel의 기능을 테스트하기 위함이기 때문에
+ * 똑같이 ArtRepositoryInterface을 상속받으며
+ * 실제 Room을 사용하지 않지만 비슷한 동작을 구현했다
+ * 실제 Room을 사용하지 않기 때문에 실제 Repository 처럼 dependency를 구현할 필요가 없다
+ * */
+class FakeArtRepository : ArtRepositoryInterface {
+
+    private val arts = mutableListOf<Art>()
+    private val artsLiveData = MutableLiveData<List<Art>>(arts)
+
+    // Room을 테스트 하는 부분은 직접 데이터를 SQL에 넣을 수 없으니 list에 넣고 빼는 형식으로 테스트 했다
+    override suspend fun insertArt(art: Art) {
+        arts.add(art)
+        refreshLiveData()
+    }
+
+    override suspend fun deleteArt(art: Art) {
+        arts.remove(art)
+        refreshLiveData()
+    }
+
+    override fun getArt(): LiveData<List<Art>> {
+        return artsLiveData
+    }
+
+    // 이미지를 찾는 테스트는 의미 없는 ImageResponse를 반환하도록 했다
+    override suspend fun searchImage(imageString: String): Resource<ImageResponse> {
+        return Resource.success(ImageResponse(listOf(),0,0))
+    }
+
+    private fun refreshLiveData() {
+        artsLiveData.postValue(arts)
+    }
+}
---
+++ app/src/test/java/com/atilsamancioglu/artbookhilttesting/viewmodel/ArtViewModelTest.kt
@@ -0,0 +1,67 @@
+package com.atilsamancioglu.artbookhilttesting.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.atilsamancioglu.artbookhilttesting.MainCoroutineRule
+import com.atilsamancioglu.artbookhilttesting.getOrAwaitValueTest
+import com.atilsamancioglu.artbookhilttesting.repo.FakeArtRepository
+import com.atilsamancioglu.artbookhilttesting.util.Status
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * viewModel에 대한 테스트
+ * */
+@ExperimentalCoroutinesApi
+class ArtViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    /*
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+     */
+
+    // 똑같이 사용중인 viewModel에 대한 변수를 선언한다
+    private lateinit var viewModel: ArtViewModel
+
+    @Before
+    fun setup() {
+        // 실제 Repository처럼 Room을 사용하지 않고 Test 하기 때문에
+        // 의존성을 부여할 필요가 없는 ViewModel을 만들 수 있다
+        // 간단하게 viewModel에 있는 기능을 테스트 하기 위함이다
+        viewModel = ArtViewModel(FakeArtRepository())
+    }
+
+    @Test
+    fun `insert art without year returns error`() {
+        viewModel.makeArt("Mona Lisa", "Da Vinci", "")
+
+        val value = viewModel.insertArtMessage.getOrAwaitValueTest()
+
+        assertThat(value.status).isEqualTo(Status.ERROR)
+    }
+
+
+    @Test
+    fun `insert art without name returns error`() {
+        viewModel.makeArt("", "Da Vinci", "1500")
+
+        val value = viewModel.insertArtMessage.getOrAwaitValueTest()
+
+        assertThat(value.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert art without artistName returns error`() {
+        viewModel.makeArt("Mona Lisa", "", "1500")
+
+        val value = viewModel.insertArtMessage.getOrAwaitValueTest()
+
+        assertThat(value.status).isEqualTo(Status.ERROR)
+    }
+}
